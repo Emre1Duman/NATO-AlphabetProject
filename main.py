0b51331803413b39cabbc19a043d1f607cf9e165
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-10-27 17:09:43 +0100

    Updated with Excpetion Handling

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()} #Formating into an organised dictionary using comprehension
 
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        Output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(Output_list)
 
-word = input("Enter a word: ").upper()
-
-Output_list = [phonetic_dict[letter] for letter in word]
-print(Output_list)
+generate_phonetic()
